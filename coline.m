27a7955568df5a9cdf29897259a4036750b37504
coljacob <coline1996@gmail.com>
2017-03-11 11:43:04 +0100

    update coline

--- coline.m
+++ coline.m
@@ -8,7 +8,7 @@ Jp = 10;
 Omega = 0:400;
 cr1 = 04; cr2 = 05; cr3 = 0; cr4 = 0;
 cn1 = 10; cn2 = 01; cn3 = 0; cn4 = 09;
-k11 = 500; k12 =1380; k21 = k12; k22 = 299;
+k11 = 500; k12 = 1340; k21 = k12; k22 = 299;
 
 % **** Matrices **** %
 M = [m 0; 0 Jt];
@@ -23,13 +23,14 @@ for n = 1:length(Omega)
 G=[0 0; 0 -i*Omega(n)*Jp];
 eq_mat = M*s^2 + (C+G)*s + K;
 sys = inv(eq_mat);
-poles_system = [poles_system  pole(sys)];
+poles = pole(sys);
+poles_system = [poles_system  wkeep(poles, length(poles)/2, 'l')];
 % Appends poles to matrix 
 end
 
 
 % **** Plot CAMPBELL DIAGRAM **** %
-if size(poles_system,1) == 4        % If 2 poles
+if size(poles_system,1) == 2        % If 2 poles
     subplot(2,1,1)
     plot(Omega, real(poles_system(1,:)), 'b', Omega, real(poles_system(2,:)), 'r');
     title('Diagramme de Campbell');
@@ -40,18 +41,12 @@ if size(poles_system,1) == 4        % If 2 poles
     plot(Omega, imag(poles_system(1,:)), 'b', Omega, imag(poles_system(2,:)), 'r');   
     xlabel('Vitesse de rotation');
     ylabel('Im(s)');
-
     
-    elseif size(poles_system,1) == 6
+elseif size(poles_system,1) == 3
     subplot(2,1,1)
     plot(Omega, real(poles_system(1,:)), 'b', Omega, real(poles_system(2,:)), 'r', Omega, real(poles_system(3,:)), 'g');
     title('Diagramme de Campbell');
     xlabel('Vitesse de rotation');
-<<<<<<< HEAD:Campbell_Coline.m
-end
-
-    
-=======
     ylabel('Re(s)');
     
     subplot(2,1,2)
@@ -59,7 +54,7 @@ end
     xlabel('Vitesse de rotation');
     ylabel('Im(s)');
     
-elseif size(poles_system,1) == 8        % If 4 poles
+elseif size(poles_system,1) == 4        % If 4 poles
     figure
     subplot(2,1,1)
     plot(Omega, real(poles_system(1,:)), 'b', Omega, real(poles_system(2,:)), 'r', Omega, real(poles_system(3,:)), 'g', Omega, real(poles_system(4,:)), 'k');
@@ -74,4 +69,3 @@ elseif size(poles_system,1) == 8        % If 4 poles
     ylabel('Im(s)');
 end
 
->>>>>>> origin/master:Campbell Coline
